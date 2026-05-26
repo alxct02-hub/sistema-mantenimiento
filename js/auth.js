fd82a8a7ea13d581c505e4543240bf269d67d5f3
alxct02-hub <alxct02@gmail.com>
2026-05-26 17:27:41 -0600

    Sistema login usuarios y roles

--- js/auth.js
+++ js/auth.js
@@ -1,51 +1,138 @@
-// Importar auth
-import { auth } from './firebase-config.js';
+import { db } from './firebase-config.js';
 
 import {
 
-  signInWithEmailAndPassword
+    collection,
+    getDocs
 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Formulario
-const loginForm = document.getElementById('loginForm');
+// ======================================
+// LOGIN
+// ======================================
 
-// Evento login
-loginForm.addEventListener('submit', iniciarSesion);
+document.getElementById(
 
-// Función login
-async function iniciarSesion(e){
+    'loginForm'
 
-    e.preventDefault();
+)
 
-    // Obtener datos
-    const email = document.getElementById('email').value;
+.addEventListener(
 
-    const password = document.getElementById('password').value;
+    'submit',
 
-    try{
+    async(e) => {
 
-        // Mostrar en consola
-        console.log("Intentando iniciar sesión...");
+        e.preventDefault();
 
-        // Login Firebase
-        const userCredential = await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
+        const usuario = document.getElementById(
 
-        console.log("Login correcto:", userCredential.user);
+            'usuario'
 
-        // Redireccionar
-        window.location.href = "./pages/dashboard.html";
+        ).value;
 
-    }catch(error){
+        const password = document.getElementById(
 
-        console.error("ERROR FIREBASE:", error);
+            'password'
 
-        alert(error.message);
+        ).value;
+
+        try{
+
+            const snapshot = await getDocs(
+
+                collection(db, 'usuarios')
+
+            );
+
+            let acceso = false;
+
+            snapshot.forEach((doc) => {
+
+                const user = doc.data();
+
+                // VALIDAR
+                if(
+
+                    user.usuario === usuario &&
+
+                    user.password === password
+
+                ){
+
+                    acceso = true;
+
+                    // GUARDAR SESIÓN
+                    localStorage.setItem(
+
+                        'usuario',
+
+                        JSON.stringify(user)
+
+                    );
+
+                    // REDIRECCIÓN
+                    if(user.rol === 'director'){
+
+                        window.location.href =
+
+                        './pages/dashboard.html';
+
+                    }
+
+                    if(user.rol === 'supervisor'){
+
+                        window.location.href =
+
+                        './pages/nueva-orden.html';
+
+                    }
+
+                }
+
+            });
+
+            // ERROR
+            if(!acceso){
+
+                mostrarError(
+
+                    'Usuario o contraseña incorrectos'
+
+                );
+
+            }
+
+        }catch(error){
+
+            console.error(error);
+
+            mostrarError(
+
+                'Error iniciando sesión'
+
+            );
+
+        }
 
     }
+
+);
+
+// ======================================
+// ERROR
+// ======================================
+
+function mostrarError(texto){
+
+    const error = document.getElementById(
+
+        'error'
+
+    );
+
+    error.classList.remove('hidden');
+
+    error.innerText = texto;
 
 }
