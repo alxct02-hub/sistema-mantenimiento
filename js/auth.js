6aed3415106fc0db2d4025f0054cc7a31f0bf70f
alxct02-hub <alxct02@gmail.com>
2026-05-25 21:30:08 -0600

    Renombrado firebase-config y actualización imports

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,49 @@
-const loginForm = document.getElementById('loginForm');
+// Importar Auth
+// import { auth } from './firebase-config.js';
+
+import {
+
+    signInWithEmailAndPassword
+    
+    } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+    
+    // Formulario
+    const loginForm = document.getElementById('loginForm');
+    
+    // Evento login
+    loginForm.addEventListener('submit', iniciarSesion);
+    
+    // Función login
+    async function iniciarSesion(e){
+    
+        e.preventDefault();
+        
+            // Obtener datos
+                const email = document.getElementById('email').value;
+                
+                    const password = document.getElementById('password').value;
+                    
+                        try{
+                        
+                                // Iniciar sesión
+                                        await signInWithEmailAndPassword(
+                                                    auth,
+                                                                email,
+                                                                            password
+                                                                                    );
+                                                                                    
+                                                                                            // Redireccionar
+                                                                                                    window.location.href = "./pages/dashboard.html";
+                                                                                                    
+                                                                                                        }catch(error){
+                                                                                                        
+                                                                                                                console.error(error);
+                                                                                                                
+                                                                                                                        alert("Error al iniciar sesión");
+                                                                                                                        
+                                                                                                                            }
+                                                                                                                            
+                                                                                                                            }onst loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', async (e) => {
 
