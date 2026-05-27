3f7038ec2a9296fc0980b74f2529abd01d07153d
alxct02-hub <alxct02@gmail.com>
2026-05-27 14:59:23 -0600

    Implementación completa módulo nueva orden

--- js/ordenes.js
+++ js/ordenes.js
@@ -1,12 +1,12 @@
-// Importar Firebase
-import { database } from './firebase-config.js';
+// Importar Firestore
+import { db } from './firebase-config.js';
 
 import {
 
-    ref,
-    push
+    collection,
+    addDoc
 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Formulario
 const form = document.getElementById('ordenForm');
@@ -14,16 +14,36 @@ const form = document.getElementById('ordenForm');
 // Mensaje
 const mensaje = document.getElementById('mensaje');
 
+// Input folio
+const folioInput = document.getElementById('folio');
+
+// Generar folio único
+function generarFolio(){
+
+    const numero = Date.now();
+
+    return `OS-${numero}`;
+
+}
+
+// Asignar folio inicial
+folioInput.value = generarFolio();
+
 // Evento guardar
 form.addEventListener('submit', guardarOrden);
 
+// Guardar orden
 async function guardarOrden(e){
 
     e.preventDefault();
 
     try{
 
-        // Obtener valores
+        // Capturar datos
+        const folio = document.getElementById('folio').value;
+
+        const area = document.getElementById('area').value;
+
         const equipo = document.getElementById('equipo').value;
 
         const tecnico = document.getElementById('tecnico').value;
@@ -35,17 +55,16 @@ async function guardarOrden(e){
         const descripcion = document.getElementById('descripcion').value;
 
         // Fecha automática
-        const fecha = new Date().toLocaleDateString();
+        const fecha = new Date().toLocaleString();
 
         // Estado inicial
         const estado = "Pendiente";
 
-        // Referencia Firebase
-        const ordenesRef = ref(database, 'ordenes');
+        // Guardar Firestore
+        await addDoc(collection(db, 'ordenes'), {
 
-        // Guardar
-        await push(ordenesRef, {
-
+            folio,
+            area,
             equipo,
             tecnico,
             tipo,
@@ -62,11 +81,14 @@ async function guardarOrden(e){
         // Limpiar formulario
         form.reset();
 
+        // Nuevo folio
+        folioInput.value = generarFolio();
+
     }catch(error){
 
         console.error(error);
 
-        mensaje.innerHTML = "❌ Error al guardar";
+        mensaje.innerHTML = "❌ Error al guardar orden";
 
     }
 
