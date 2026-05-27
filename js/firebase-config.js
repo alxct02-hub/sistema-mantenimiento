fb154028c5dbf6f81ad5562df4b44403c8874feb
alxct02-hub <alxct02@gmail.com>
2026-05-27 17:15:02 -0600

    Restaurar firebase-config estable

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,49 +1,56 @@
-// ======================================
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
 // CONFIGURACIÓN FIREBASE
-// ======================================
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyDR4D1IyOqAYOE3JZzsqBhlfpwGwmT4m-A",
+  apiKey: "AIzaSyDR4D1IyOqAYOE3JZzsqBhlfpwGwmT4m-A",
 
-    authDomain:
-    "mantenimiento-planta-adbfc.firebaseapp.com",
+  authDomain:
+  "mantenimiento-planta-adbfc.firebaseapp.com",
 
-    projectId:
-    "mantenimiento-planta-adbfc",
+  projectId:
+  "mantenimiento-planta-adbfc",
 
-    storageBucket:
-    "mantenimiento-planta-adbfc.firebasestorage.app",
+  storageBucket:
+  "mantenimiento-planta-adbfc.firebasestorage.app",
 
-    messagingSenderId:
-    "182924583347",
+  messagingSenderId:
+  "182924583347",
 
-    appId:
-    "1:182924583347:web:96531f94bc101af2a12bc6",
+  appId:
+  "1:182924583347:web:96531f94bc101af2a12bc6",
 
-    measurementId:
-    "G-HZ8W5B6ZG3"
+  measurementId:
+  "G-HZ8W5B6ZG3"
 
 };
 
-// ======================================
-// INICIALIZAR FIREBASE
-// ======================================
+// INICIALIZAR
 
-firebase.initializeApp(
-    firebaseConfig
-);
+const app =
+initializeApp(firebaseConfig);
 
-// ======================================
-// FIRESTORE
-// ======================================
+// SERVICIOS
 
 const db =
-firebase.firestore();
-
-// ======================================
-// AUTH
-// ======================================
+getFirestore(app);
 
 const auth =
-firebase.auth();
+getAuth(app);
+
+// EXPORTAR
+
+export {
+  db,
+  auth
+};
